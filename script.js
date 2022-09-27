50d75a0a81041a855aeef5c9bbfbb14f25e47174
Gordo <gordonhdo@gmail.com>
2022-09-26 21:34:57 -0700

    assign logic to get keys and values

--- script.js
+++ script.js
@@ -12,22 +12,31 @@ const states = {
   g: "gold",
 };
 
+// setup on load
 window.addEventListener("load", () => {
   const keys = document.getElementById("keys");
   const values = document.getElementById("values");
+
+  // assign individual key values to keys
   Object.keys(states).map((key) => {
     const option = document.createElement("option");
     option.innerHTML = key;
     keys.appendChild(option);
   });
+
+  // assign individual value for each key value
   Object.values(states).map((value) => {
     const option2 = document.createElement("option");
     option2.innerHTML = value;
     values.appendChild(option2);
   });
+
+  // change values on key change
   keys.addEventListener("change", () => {
     values.value = states[keys.value];
   });
+
+  // change keys on value change
   values.addEventListener("change", () => {
     keys.value = Object.keys(states).filter(
       (state) => states[state] === values.value
